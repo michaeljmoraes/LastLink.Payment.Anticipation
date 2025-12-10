19e77ec7e9053a1261f1f34b817e05cf46e5087c
michaeljmoraes <michaeljmoraes@gmail.com>
2025-12-10 01:37:44 -0300

    lastlink.payment.aticipation
    frontend environment adjusts

--- frontend/lastlink-payment-front/src/environments/environment.docker.ts
+++ frontend/lastlink-payment-front/src/environments/environment.docker.ts
@@ -1,5 +1,5 @@
 export const environment = {
   name: 'docker',
   production: true,
-  apiUrl: 'http://api:8080/api/v1'
+  apiUrl: 'http://api:8080/api/v1/anticipations'
 };
--- frontend/lastlink-payment-front/src/environments/environment.ts
+++ frontend/lastlink-payment-front/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5274/api/v1'
+  apiUrl: 'http://localhost:5274/api/v1/anticipations'
 };
